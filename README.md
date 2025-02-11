7f5fcd49f50fb2fc967458aec804ad5e1b533fd8
John Groton <john.groton@mymail.champlain.edu>
2025-02-11 18:14:38 -0500

    making basic anim

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# JuliaDemo" 
+
+To install dependencies run 
+
+1: import Pkg; Pkg.add("Plots")
+2: import Pkg; Pkg.add("PlotThemes")
+3: import Pkg; Pkg.add("ImageMagick")
+
+it should create a file called anim.gif and that can be seen when run in visual studio code
--- test.jl
+++ test.jl
@@ -1,9 +1,30 @@
 using Plots
 using PlotThemes
+using ImageMagick
 
+function main()
+    
 theme(:dracula ::Symbol;)
 
-f(x) = 10*sin(x)
-g(x) = x^2
+# equations
+x(t) = sin(t)
+y(t) = cos(t)
 
-plot([f,g], 0, 2*pi, title="f and g plot")
+# time frame
+t = 0:0.1:10
+n = length(t)
+
+xpos = x.(t)
+ypos = y.(t)
+
+anim = @animate for i in 1:n
+    scatter!([xpos[i]], [ypos[i]], legend=:none, title="Trajectory",
+    markersize=2, xaxis=("x", (-5, 5)), yaxis=("y", (-5,5)))
+
+end
+
+gif(anim, "anim.mp4", fps=15)
+
+end
+
+main()
