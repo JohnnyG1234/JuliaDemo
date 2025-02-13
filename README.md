63a86f4328772aad286c1e10daaaec933cd11014
John Groton <john.groton@mymail.champlain.edu>
2025-02-13 14:23:38 -0500

    messing around

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 To install dependencies run 
 
 1: import Pkg; Pkg.add("Plots")
+
 2: import Pkg; Pkg.add("PlotThemes")
+
 3: import Pkg; Pkg.add("ImageMagick")
 
 it should create a file called anim.gif and that can be seen when run in visual studio code
--- test.jl
+++ test.jl
@@ -2,29 +2,43 @@ using Plots
 using PlotThemes
 using ImageMagick
 
-function main()
+gr()
+
+function circle_anim()
+    theme(:dracula ::Symbol;)
+
+    # equations
+    x(t) = sin(t)
+    y(t) = cos(t)
     
-theme(:dracula ::Symbol;)
-
-# equations
-x(t) = sin(t)
-y(t) = cos(t)
-
-# time frame
-t = 0:0.1:10
-n = length(t)
-
-xpos = x.(t)
-ypos = y.(t)
-
-anim = @animate for i in 1:n
-    scatter!([xpos[i]], [ypos[i]], legend=:none, title="Trajectory",
-    markersize=2, xaxis=("x", (-5, 5)), yaxis=("y", (-5,5)))
-
+    # time frame
+    t = 0:0.1:10
+    n = length(t)
+    
+    xpos = x.(t)
+    ypos = y.(t)
+    
+    anim = @animate for i in 1:n
+        scatter!([xpos[i]], [ypos[i]], legend=:none, title="Trajectory",
+        markersize=2, xaxis=("x", (-5, 5)), yaxis=("y", (-5,5)))
+    
+    end
+    
+    gif(anim, "anim.mp4", fps=15)
 end
 
-gif(anim, "anim.mp4", fps=15)
 
+function histo()
+    theme(:dracula ::Symbol;)
+    n = 100
+    anim = @animate for i in 1:n
+        histogram2d(randn(5), randn(5), nbins = 20, xaxis=("x", (-5, 5)), yaxis=("y", (-5,5)))
+    end
+    gif(anim, "anim.mp4", fps=15)
 end
 
+
+function main()
+    @time histo()
+end
 main()
